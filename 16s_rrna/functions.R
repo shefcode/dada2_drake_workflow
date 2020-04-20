3628bc3f411096fb2a5c6ba2f89a87bdcf388242
shefcode <62350473+shefcode@users.noreply.github.com>
2020-04-20 11:34:19 -0700

    Update functions.R

--- 16s_rrna/functions.R
+++ 16s_rrna/functions.R
@@ -1,9 +1,2 @@
 # Functions
 # functions.R
-
-# Make a function with upsetR
-# Make a function with ggplot
-# Make a function to graph all the results from the taxonomy portion
-# Make a function to select which samples to graph
-# Try to play with the loops and if & else statements
-# Go through R for Data Science [Functions] to get a better understanding
