9bb9281c658b199956ba347821ca55c78f56b109
Shefali Vishwakarma <62350473+shefcode@users.noreply.github.com>
2020-04-24 14:27:28 -0700

    Update dada2_drake_workflow.R
    Addition for metadata, plots, astrobiomike tutorial [Taxonomic Summaries]

--- 16s_rrna/dada2_drake_workflow.R
+++ 16s_rrna/dada2_drake_workflow.R
@@ -2,6 +2,12 @@
 
 source("packages.R")
 
+# Pull metadata for the 11 samples for "filtered_bee_hind_gut_pellet" [Need to incorporate into drake]
+load_meta <- read.csv("meta56_66.csv")
+rownames(load_meta) <- load_meta$MBI.Sample.ID
+meta <- select(load_meta, -c(MBI.Sample.ID))
+
+# drake script
 dada2analysis <- drake_plan(
   set_path = here("raw_sequences/samples/filtered_bee_hind_gut_pellet"),
   files = list.files(set_path, pattern = "fastq.gz"),
@@ -30,48 +36,63 @@ dada2analysis <- drake_plan(
     set_rownames(sample.names),
   taxa = assignTaxonomy(seqtab.nochim, here("taxa/silva_nr_v138_train_set.fa.gz"), multithread=TRUE),
   taxa_sp = addSpecies(taxa, here("taxa/silva_species_assignment_v138.fa.gz")),
-  taxa.print = set_rownames(taxa_sp, NULL)
+  taxa.print = set_rownames(taxa_sp, NULL),
+  ps = phyloseq(otu_table(seqtab.nochim, taxa_are_rows=FALSE), sample_data(meta), tax_table(taxa)),
+  dna = Biostrings::DNAStringSet(taxa_names(ps)),
+  dna_n = setNames(dna, taxa_names(ps)),
+  ps_m = merge_phyloseq(ps, dna_n),
+  ps_t = setNames(taxa_names(ps_m), paste0("ASV", seq(ntaxa(ps_m))))
 )
 
 make(dada2analysis)
 
-loadd(set_path, files, fnFs, fnRs, sample.names, filtFs, filtRs, out,
-      errF, errR, derepFs, derepRs, dadaFs, dadaRs, mergers, seqtab,
-      seqtab.nochim, getN, track, taxa, taxa_sp, taxa.print)
-
-loadd(seqtab.nochim, taxa.print, taxa)
-
-print_df <- as.data.frame(taxa.print)
-
+loadd(set_path, files, fnFs, fnRs, sample.names, filtFs, filtRs, out, errF, errR, derepFs, derepRs, dadaFs, dadaRs,
+      mergers, seqtab, seqtab.nochim, getN, track, taxa, taxa_sp, taxa.print, ps, dna, dna_n, ps_m, ps_t)
 vis_drake_graph(dada2analysis)
-
-####
-
-write.csv(print_df, here("filtered_bee_hind_gut_pellet_tax.csv"), row.names = FALSE)
-
-view(taxa.print)
-vis_drake_graph(dada2analysis)
-
-# metadata
-
-meta <- read.csv("meta56_66.csv")
-rownames(meta) <- meta[, 1]
-meta[,1] <- NULL
 
 # Addition of phloseq
 # following the DADA2 tutorial
 
 theme_set(theme_bw())
 
-ps <- phyloseq(otu_table(seqtab.nochim, taxa_are_rows=FALSE),
-               sample_data(meta),
-               tax_table(taxa))
-
+ps <- phyloseq(otu_table(seqtab.nochim, taxa_are_rows=FALSE), sample_data(meta), tax_table(taxa))
 
 dna <- Biostrings::DNAStringSet(taxa_names(ps))
-names(dna) <- taxa_names(ps)
-ps <- merge_phyloseq(ps, dna)
-taxa_names(ps) <- paste0("ASV", seq(ntaxa(ps)))
-ps
+names(dna) <- taxa_names(ps) # dna_n
+ps <- merge_phyloseq(ps, dna) # ps_m
+taxa_names(ps) <- paste0("ASV", seq(ntaxa(ps))) # ps_t
+ps #
 
-plot_richness(ps)
+plot_richness(ps, measures=c("Chao1", "Shannon", "Simpson")) # Plot alpha diversity [Works]
+
+# Ordinate
+ps.prop <- transform_sample_counts(ps, function(otu) otu/sum(otu))
+ord.nmds.bray <- ordinate(ps.prop, method="NMDS", distance="bray")
+
+plot_ordination(ps.prop, ord.nmds.bray, color="When", title="Bray NMDS")
+
+# Bar Plot
+top20 <- names(sort(taxa_sums(ps), decreasing=TRUE))[1:20]
+ps.top20 <- transform_sample_counts(ps, function(OTU) OTU/sum(OTU))
+ps.top20 <- prune_taxa(top20, ps.top20)
+plot_bar(ps.top20, fill = "Family")
+                                    
+########### astrobiomike tutorial  [Taxonomic Summaries] --- In Progress
+phyla_counts_tab <- otu_table(tax_glom(ps, taxrank="Phylum")) 
+phyla_counts_tab
+phyla_tax_vec <- as.vector(tax_table(tax_glom(ps, taxrank="Phylum"))[,2]) 
+colnames(phyla_counts_tab) <- as.vector(phyla_tax_vec)
+unclassified_tax_counts <- colSums(seqtab.nochim) - colSums(phyla_counts_tab)
+phyla_and_unidentified_counts_tab <- rbind(phyla_counts_tab, "Unclassified"=unclassified_tax_counts)
+phyla_and_unidentified_counts_tab
+temp_major_taxa_counts_tab <- phyla_and_unidentified_counts_tab[!row.names(phyla_and_unidentified_counts_tab) %in% "Proteobacteria", ]
+class_counts_tab <- otu_table(tax_glom(ps, taxrank="Class")) 
+class_tax_phy_tab <- tax_table(tax_glom(ps, taxrank="Class")) 
+phy_tmp_vec <- class_tax_phy_tab[,2]
+class_tmp_vec <- class_tax_phy_tab[,3]
+rows_tmp <- row.names(class_tax_phy_tab)
+class_tax_tab <- data.frame("Phylum"=phy_tmp_vec, "Class"=class_tmp_vec, row.names = rows_tmp)
+proteo_classes_vec <- as.vector(class_tax_tab[class_tax_tab$Phylum == "Proteobacteria", "Class"])
+colnames(class_counts_tab) <- as.vector(class_tax_tab$Class) 
+proteo_class_counts_tab <- class_counts_tab[colnames(class_counts_tab) %in% proteo_classes_vec, ] 
+proteo_no_class_annotated_counts <- phyla_and_unidentified_counts_tab[colnames(phyla_and_unidentified_counts_tab) %in% "Proteobacteria", ] - colSums(proteo_class_counts_tab)
