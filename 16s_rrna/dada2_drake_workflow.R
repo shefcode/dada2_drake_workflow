2e1abbb672a3ad20dcc323ce0206fa01324edc35
Shefali Vishwakarma <62350473+shefcode@users.noreply.github.com>
2020-04-24 10:12:30 -0700

    Update dada2_drake_workflow.R

--- 16s_rrna/dada2_drake_workflow.R
+++ 16s_rrna/dada2_drake_workflow.R
@@ -1,17 +1,18 @@
-# Drake script
+# Analyzing 16S Microbiome Insight data
 
-source("package.R")
-source("functions.R")
+source("packages.R")
 
 dada2analysis <- drake_plan(
-  set_path = here("../data/"),
-  files = list.files(set_path, pattern = ".fastq.gz"),
-  fnFs = sort(list.files(set_path, pattern="1.fastq.gz", full.names = TRUE)),
-  fnRs = sort(list.files(set_path, pattern="2.fastq.gz", full.names = TRUE)),
+  set_path = here("raw_sequences/samples/filtered_bee_hind_gut_pellet"),
+  files = list.files(set_path, pattern = "fastq.gz"),
+  fnFs = sort(list.files(set_path, pattern = "R1_001.fastq.gz", full.names = TRUE)),
+  fnRs = sort(list.files(set_path, pattern = "R2_001.fastq.gz", full.names = TRUE)),
   sample.names = sapply(strsplit(basename(fnFs), "_"), `[`, 1),
   filtFs = file.path(set_path, "filtered", paste0(sample.names, "_F_filt.fastq.gz")),
   filtRs = file.path(set_path, "filtered", paste0(sample.names, "_R_filt.fastq.gz")),
-  out = filterAndTrim(fnFs, filtFs, fnRs, filtRs, truncLen=c(240,160), maxN=0, maxEE=c(2,30), truncQ=2, rm.phix=TRUE, compress=TRUE, multithread=FALSE),
+  out = filterAndTrim(fnFs, filtFs, fnRs, filtRs, truncLen=c(240,160),
+                      maxN=0, maxEE=c(2,30), truncQ=2, rm.phix=TRUE,
+                      compress=TRUE, multithread=FALSE),
   errF = learnErrors(filtFs, multithread=TRUE),
   errR = learnErrors(filtRs, multithread=TRUE),
   derepFs = derepFastq(filtFs, verbose=TRUE) %>% 
@@ -27,31 +28,50 @@ dada2analysis <- drake_plan(
   track = cbind(out, sapply(dadaFs, getN), sapply(dadaRs, getN), sapply(mergers, getN), rowSums(seqtab.nochim)) %>% 
     set_colnames(c("input", "filtered", "denoisedF", "denoisedR", "merged", "nonchim")) %>% 
     set_rownames(sample.names),
-  taxa = assignTaxonomy(seqtab.nochim, here("../taxa/silva_nr_v138_train_set.fa.gz"), multithread=TRUE),
-  taxa_sp = addSpecies(taxa, here("../taxa/silva_species_assignment_v138.fa.gz")),
+  taxa = assignTaxonomy(seqtab.nochim, here("taxa/silva_nr_v138_train_set.fa.gz"), multithread=TRUE),
+  taxa_sp = addSpecies(taxa, here("taxa/silva_species_assignment_v138.fa.gz")),
   taxa.print = set_rownames(taxa_sp, NULL)
 )
 
 make(dada2analysis)
-loadd(set_path, files, fnFs, fnRs, sample.names, filtFs, filtRs, out, errF, errR, derepFs, derepRs,dadaFs, dadaRs, mergers, seqtab, seqtab.nochim, getN, track, taxa, taxa_sp, taxa.print)
 
-#### phyloseq?
+loadd(set_path, files, fnFs, fnRs, sample.names, filtFs, filtRs, out,
+      errF, errR, derepFs, derepRs, dadaFs, dadaRs, mergers, seqtab,
+      seqtab.nochim, getN, track, taxa, taxa_sp, taxa.print)
 
-library(phyloseq)
+loadd(seqtab.nochim, taxa.print, taxa)
 
-plot_richness(taxa)
-
-# Analysis tools
-plotQualityProfile(fnFs[1:2])
-plotQualityProfile(fnRs[1:2])
-head(out)
-plotErrors(errF, nominalQ=TRUE)
-dadaFs[[1]]
-head(mergers[[1]])
-dim(seqtab)
-table(nchar(getSequences(seqtab)))
-dim(seqtab.nochim)
-sum(seqtab.nochim)/sum(seqtab)
-head(track)
+print_df <- as.data.frame(taxa.print)
 
 vis_drake_graph(dada2analysis)
+
+####
+
+write.csv(print_df, here("filtered_bee_hind_gut_pellet_tax.csv"), row.names = FALSE)
+
+view(taxa.print)
+vis_drake_graph(dada2analysis)
+
+# metadata
+
+meta <- read.csv("meta56_66.csv")
+rownames(meta) <- meta[, 1]
+meta[,1] <- NULL
+
+# Addition of phloseq
+# following the DADA2 tutorial
+
+theme_set(theme_bw())
+
+ps <- phyloseq(otu_table(seqtab.nochim, taxa_are_rows=FALSE),
+               sample_data(meta),
+               tax_table(taxa))
+
+
+dna <- Biostrings::DNAStringSet(taxa_names(ps))
+names(dna) <- taxa_names(ps)
+ps <- merge_phyloseq(ps, dna)
+taxa_names(ps) <- paste0("ASV", seq(ntaxa(ps)))
+ps
+
+plot_richness(ps)
